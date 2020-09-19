9a8f989bc9ebfe66cad0c20f7dd93d2e8cb4331e
vachanbiyani <67270111+vachanbiyani@users.noreply.github.com>
2020-09-19 08:29:40 +0530

    Add files via upload

---
+++ Bat Man/umbrella.js
@@ -0,0 +1,17 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("Images/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+    }
+
+    display(){
+        
+        imageMode(CENTER);
+        image(this.image,this.umbrella.position.x,this.umbrella.position.y+70,300,300);
+    }
+}
